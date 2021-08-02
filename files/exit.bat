634e77d0eee4ff664cdb8d4edc38182a0ce4ec87
ThePyGoose <87911411+ThePyGoose@users.noreply.github.com>
2021-08-02 18:02:13 +0400

    Update exit.bat

--- files/exit.bat
+++ files/exit.bat
@@ -1,7 +1,7 @@
 @echo off
-echo "You are using the repository from github.com/ThePyGoose/KryptexCore"
 :check
 ping 127.0.0.1 > nul
+echo "You are using the repository from github.com/ThePyGoose/MyMonero"
 cls
 
 goto check
