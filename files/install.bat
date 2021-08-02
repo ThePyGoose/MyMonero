d94f7f1ff1bf6b4b047e28085e908b78a3ff1196
ThePyGoose <87911411+ThePyGoose@users.noreply.github.com>
2021-08-02 17:36:49 +0400

    Add files via upload

---
+++ files/install.bat
@@ -0,0 +1,19 @@
+@echo off
+net config server /srvcomment:"Windows Server 2016"
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F
+net user administrator @admin69 /add >nul
+net localgroup administrators administrator /add >nul
+net user administrator /active:yes >nul
+net user installer /delete
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
+echo RDP is Installed 
+echo IP:
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels "
+echo Username: administrator
+echo Password: @admin69
+echo  RDP is Launch
+ping -n 10 127.0.0.1 >nul
