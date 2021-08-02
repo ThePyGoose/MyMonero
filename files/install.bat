d1fbdb6476571fbdc6a872d103c838a0e45f8e08
ThePyGoose <87911411+ThePyGoose@users.noreply.github.com>
2021-08-02 17:56:23 +0400

    Update install.bat

--- files/install.bat
+++ files/install.bat
@@ -10,10 +10,4 @@ sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
-echo RDP is Installed 
-echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels "
-echo Username: administrator
-echo Password: @admin69
-echo  RDP is Launch
-ping -n 10 127.0.0.1 >nul
+echo "You are using the repository from github.com/ThePyGoose/KryptexCore"
